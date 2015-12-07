4e6cb05d3df81d6af72836a7a1ca0a5003731bed
Christian <Bryant>
2015-12-07 12:42:43 -0500

    pushing more code

--- js/philipsearch.js
+++ js/philipsearch.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
   hideAllTemplates();
+  hideAllPhils();
   $("#tmpFitness").show();
-  $(".fitness").css("color","orange");
+  $(".fitness").css("color","#f4ff81");
+  $("#img_FitPhil").show();
 
 
   function hideAllTemplates(){
@@ -12,32 +14,49 @@ $(document).ready(function(){
     $("#tmpConnect").hide();
   }
 
+  function hideAllPhils(){
+    $("#img_NutritionPhil").hide();
+    $("#img_LifePhil").hide();
+    $("#img_FitPhil").hide();
+  }
+
   function resetLinkColor(){
     $(".fitness").css("color","white");
     $(".nutrition").css("color","white");
     $(".life").css("color","white");
+    $(".nutrition").removeClass("light-blue-text text-lighten-3");
+    $(".life").removeClass("pink-text");
   }
 
   $(".fitness").click(function(){
     hideAllTemplates();
+    hideAllPhils();
     resetLinkColor();
-    $(".fitness").css("color","orange");
+    $(".fitness").css("color","#f4ff81");
     $("#tmpFitness").show();
+      $("#img_FitPhil").show();
   })
   $(".nutrition").click(function(){
     hideAllTemplates();
+    hideAllPhils();
     resetLinkColor();
-    $(".nutrition").css("color","orange");
+    $(".nutrition").addClass("light-blue-text text-lighten-3");
     $("#tmpNutrition").show();
+    $("#img_NutritionPhil").show();
   })
   $(".life").click(function(){
     hideAllTemplates();
+    hideAllPhils();
     resetLinkColor();
     $(".life").css("color","orange");
     $("#tmpLife").show();
+    $(".life").addClass("pink-text");
+    $("#img_LifePhil").show();
   })
+
   $(".connect").click(function(){
     hideAllTemplates();
+    hideAllPhils();
     resetLinkColor();
     $("#tmpConnect").show();
   })
