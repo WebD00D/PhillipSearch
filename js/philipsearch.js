0896cd17a7f7ce0308ffcbfc37c17c96ac70da40
Christian <Bryant>
2015-12-07 10:40:28 -0500

    first build

--- js/philipsearch.js
+++ js/philipsearch.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-  
+
+  hideAllTemplates();
+  $("#tmpFitness").show();
+  $(".fitness").css("color","orange");
+
+
+  function hideAllTemplates(){
+    $("#tmpFitness").hide();
+    $("#tmpNutrition").hide();
+    $("#tmpLife").hide();
+    $("#tmpConnect").hide();
+  }
+
+  function resetLinkColor(){
+    $(".fitness").css("color","white");
+    $(".nutrition").css("color","white");
+    $(".life").css("color","white");
+  }
+
+  $(".fitness").click(function(){
+    hideAllTemplates();
+    resetLinkColor();
+    $(".fitness").css("color","orange");
+    $("#tmpFitness").show();
+  })
+  $(".nutrition").click(function(){
+    hideAllTemplates();
+    resetLinkColor();
+    $(".nutrition").css("color","orange");
+    $("#tmpNutrition").show();
+  })
+  $(".life").click(function(){
+    hideAllTemplates();
+    resetLinkColor();
+    $(".life").css("color","orange");
+    $("#tmpLife").show();
+  })
+  $(".connect").click(function(){
+    hideAllTemplates();
+    resetLinkColor();
+    $("#tmpConnect").show();
+  })
+
+
+
 })
