966e66d899028b846db29f8982b3b18bc4211a7d
Christian <Bryant>
2015-12-10 10:29:08 -0500

    push it

--- js/philipsearch.js
+++ js/philipsearch.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
   $(window).on( "orientationchange", function(event) {
 
-    alert( "This device is in " + event.orientation + " mode!" );    
+    //alert( "This device is in " + event.orientation + " mode!" );    
 
   })
 
